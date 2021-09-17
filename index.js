3cf8bf66700f589604380b8d90144da5754326af
Edilson Nascimento <bughuntmaster@outlook.com>
2021-09-16 21:05:08 -0300

    Adicionando conexão com banco de dados.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("./express/express");
 const config = require("./config/config.json");
-//const db = require();
+const db = require('./db/connector');
 
 var app = express();
 
-app.listen(config['port'], /*config['hostname'],*/ () => {
-    console.log('[+] Server iniciado.');
+db.connect(err => {
+    if(err){
+        console.log('Error: MySQL connect failed.');
+        console.log(err);
+    } else {
+        console.log('[+] Conexão com banco de dados estabelecida.');
+        app.listen(config['port'], /*config['hostname'],*/ () => {
+            console.log('[+] Aplicação iniciada.');
+        });
+    }
 });
